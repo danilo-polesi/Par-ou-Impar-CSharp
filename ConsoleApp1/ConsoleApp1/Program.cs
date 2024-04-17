c977f1c6b6e3d5fda1c7f5fd18a1a1480eea2cb8
danilo-polesi <danilopolesk8@gmail.com>
2024-04-17 17:46:55 -0300

    primeiro commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.Clear();
+            Console.Write("\n\tDigite um número inteiro:");
+            x = int.Parse(Console.ReadLine());
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("\nO número " + x + " é par");
+            }
+            else
+            {
+                Console.WriteLine("\nO número " + x + " é ímpar");
+            }
+            Console.ReadKey();
+        }
+    }
+}
